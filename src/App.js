377f93580b2d0fc3d0051098a4d05b742568a0d3
sofiapatrocinio <sofiapatrocinio@hotmail.com>
2022-02-21 21:15:16 -0300

    Add function to generate graph

--- src/App.js
+++ src/App.js
@@ -1,24 +1,90 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import GraphModel from './components/Graph';
+import { InteractiveForceGraph, ForceGraphNode, ForceGraphLink } from 'react-vis-force';
+import utils from './components/utils';
 
 function App() {
+  const [showGraph, setShowGraph] = useState(false);
+  const [showBestPath, setShowBestPath] = useState(false);
+  const currentGraph = new GraphModel(100);
+  const [nodes, setNodes] = useState({});
+  const [links, setLinks] = useState({});
+
+  const generateNodes = (graph, qtd) => {
+    const nodes = [];
+    for (let i = 0; i <= qtd; i++) {
+      graph.listAdj.set(i, []);
+      nodes.push({ id: i });
+    }
+    setNodes(nodes);
+  }
+
+  const generateLinks = (graph, qtdNodes, qtdLinks) => {
+    const min = 0;
+    let qtd = qtdLinks;
+    const links = [];
+  
+  
+    while (qtd--) {
+      const node1 = utils.randomIntFromInterval(min, qtdNodes);
+      let node2 = utils.randomIntFromInterval(min, qtdNodes);
+      const price = utils.randomIntFromInterval(min + 1, qtdNodes) * 2;
+  
+      if (node1 === node2) node2 = utils.randomIntFromInterval(min, qtdNodes);
+  
+      if (links.length === 0 || !utils.alreadyExists(links, node1, node2)) {
+        links.push({ target: node1, source: node2 });
+        graph.addDirectedLink(node1, node2, price);
+      }
+    }
+    setLinks(links);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {!showGraph ?
+        <div>
+          <div className="cover">
+            <h1 className="cover-title">Grafos 2 - Toll Less</h1>
+            <span className="cover-text">
+              Micaella Gouveia de Lima 17/0111288
+            </span>
+            <span className="cover-text">
+              Sofia Costa Patrocínio 17/0114333
+            </span>
+          </div>
+          <div className="action">
+            <button className="graph-button" type="button" onClick={() => {setShowGraph(true); generateNodes(currentGraph, 10); generateLinks(currentGraph, 10, 30)}}>
+              Gerar Grafo
+            </button>
+          </div>
+        </div> : <div className="graph">
+            <div className='action'>
+              <InteractiveForceGraph
+                simulationOptions={{ height: 500, width: 500 }}
+              // onSelectNode={(node) => console.log(node)}
+              >
+                {nodes.map(node => (
+                  <ForceGraphNode
+                    key={node.id}
+                    node={node}
+                    fill="red"
+                  />
+                ))}
+                {links.map(r => (
+                  <ForceGraphLink link={{ source: r.source, target: r.target }} />
+                ))}
+              </InteractiveForceGraph>
+            </div>
+            <div className="action">
+              <button className="graph-button mrg-right-10" type="button" onClick={() => setShowBestPath(true)}>
+                Mostrar Caminho Mais Barato
+              </button>
+            </div>
+          </div>
+      }
+    </>
   );
 }
 
--- src/components/Graph.js
+++ src/components/Graph.js
@@ -1,57 +1,35 @@
-const createGraph = () => {
-  return new Map()
+export default class GraphModel {
+  constructor(node) {
+    this.node = node;
+    this.listAdj = new Map();
+    this.links = 0;
 }
 
-// gera os nós e as listas de adj
-const generateNodes = (graph, qtd) => {
-  const nodes = [];
-  for (let id = 0; id <= qtd; id++) {
-    graph.set(id, []);
-    nodes.push({ id });
-  }
-}
-
-// cria a aresta unidirecional com preço
-const addDirectedLink = (graph, source, node, price) => {
-  graph.get(source).push({ node, price });
-}
-
-const printGraph = (graph) => {
-  const vertices = graph.keys();
-
-  for (let i of vertices) {
-    const nodes = graph.get(i);
-    let conc = "";
-
-    for (let j of nodes)
-      conc += j.node + " (price-> " + j.price + ") ";
-
-    console.log(i + " -> " + conc);
-  }
-}
-
-const generateLinks = (graph, qtdNodes, qtdLinks) => {
-  const min = 0;
-  let qtd = qtdLinks;
-  const links = [];
-
-
-  while (qtd--) {
-    const node1 = randomIntFromInterval(min, qtdNodes);
-    let node2 = randomIntFromInterval(min, qtdNodes);
-    const price = randomIntFromInterval(min + 1, qtdNodes) * 2;
-
-    if (node1 === node2) node2 = randomIntFromInterval(min, qtdNodes);
-
-    if (links.length === 0 || !alreadyExists(links, node1, node2)) {
-      links.push({ target: node1, source: node2 });
-      addDirectedLink(graph, node1, node2, price);
+  // gera os nós e as listas de adj
+  generateNodes = (graph, qtd) => {
+    const nodes = [];
+    for (let id = 0; id <= qtd; id++) {
+      graph.set(id, []);
+      nodes.push({ id });
     }
   }
-}
-export default {
-  createGraph,
-  generateNodes,
-  generateLinks,
-  printGraph
+
+  // cria a aresta unidirecional com preço
+  addDirectedLink = (source, node, price) => {
+    this.listAdj.get(source).push({ node, price });
+  }
+
+  printGraph = (graph) => {
+    const vertices = graph.keys();
+
+    for (let i of vertices) {
+      const nodes = graph.get(i);
+      let conc = "";
+
+      for (let j of nodes)
+        conc += j.node + " (price-> " + j.price + ") ";
+
+      console.log(i + " -> " + conc);
+    }
+  }
 }
